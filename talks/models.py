916cc3b866f7aa6b2a438569b373d10b4bafbf52
vishwas <vishwas.mittal78@gmail.com>
2017-09-25 14:00:50 +0530

    basic setup and added models

--- talks/models.py
+++ talks/models.py
@@ -2,4 +2,35 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    theme = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    timestamp = models.DateField()
+    image = models.ImageField(upload_to='events', blank=True, null=True)
+
+
+class Speaker(models.Model):
+    name = models.CharField(max_length=50)
+    designation = models.CharField(max_length=50)
+    date = models.DateField()
+    about = models.TextField()
+    facebook = models.URLField(null=True)
+    linkedin = models.URLField(null=True)
+    twitter = models.URLField(null=True)
+    profile_pic = models.ImageField(upload_to='speakers', null=True, blank=True)
+
+
+class Organizer(models.Model):
+    name = models.CharField(max_length=50)
+    designation = models.CharField(max_length=50)
+    facebook = models.URLField(default="https://www.facebook.com/")
+    linkedin = models.URLField(default="https://www.linkedin.com/")
+    profile_pic = models.ImageField(upload_to='speakers')
+    # twitter = models.URLField(default="https://www.twitter.com/")
+
+
+class Sponsor(models.Model):
+    name = models.CharField(max_length=50)
+    link = models.URLField()
+    image = models.ImageField(upload_to='sponsors', null=True, blank=True)
--- tedxiitr/settings.py
+++ tedxiitr/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'talks',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'tedxiitr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tedx',
+        'USER': 'tester',
+        'PASSWORD': 'tester',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
