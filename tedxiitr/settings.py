ecd36b3b5c831bf0d2c32849db542e0315a3193e
vishwas <vishwas.mittal78@gmail.com>
2017-09-27 23:05:25 +0530

    added gunicorn

--- tedxiitr/settings.py
+++ tedxiitr/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY='eq(th$czw_5jlcd=b4o!shp7vf)pmo^wnvm(u5w+z2zg8232g3'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY='eq(th$czw_5jlcd=b4o!shp7vf)pmo^wnvm(u5w+z2zg8232g3'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
