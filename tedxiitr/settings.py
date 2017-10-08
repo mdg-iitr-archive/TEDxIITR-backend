1b34a25b9ff86077c5d95819d05c23f60a14aa7e
Kush Taneja <kushtaneja16@gmail.com>
2017-10-08 15:47:12 +0530

    Resolved Static errors

--- tedxiitr/settings.py
+++ tedxiitr/settings.py
@@ -136,12 +136,11 @@ AWS_S3_HOST = 's3-us-east-2.amazonaws.com'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_LOCATION = 'static'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'tedxiitr.storage.StaticStorage'
 
 MEDIAFILES_LOCATION = 'media'
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-DEFAULT_FILE_STORAGE = 'appetizer.storages.MediaStorage'
+DEFAULT_FILE_STORAGE = 'tedxiitr.storage.StaticStorage'
